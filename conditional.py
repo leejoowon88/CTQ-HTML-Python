daf7520a550097c89c426125152bb441798e0dc6
leejoowon88 <84168850+leejoowon88@users.noreply.github.com>
2022-06-11 21:23:10 +0900

    3,4차시 과제 추가

---
+++ conditional.py
@@ -0,0 +1,17 @@
+user_id = input('id?')
+user_pwd = input('password?')
+
+'''
+if user_pwd == '111111':
+    print('Hello master')
+else:
+    print('Who are you?')
+'''
+if user_id == 'juwon' and user_pwd == '111111':
+        print('Hello master')
+elif user_id == 'juwan' and user_pwd == '333333':
+        print('Hello master')
+    else:
+        print('Who are you?')
+else:
+    print('Who are you?')
---
+++ string.py
@@ -0,0 +1,23 @@
+print("helloworld")
+print('hello world')
+print("Hell'o' world")
+#escape
+print("Hell'o' \"w\"orld")
+
+#newline
+print('h')
+print('e')
+print('l')
+print('l')
+print('o')
+
+print('H/ne/nl/nl/# NOTE:')
+
+#docstring
+print('''
+H
+e
+l
+l
+o
+''')
